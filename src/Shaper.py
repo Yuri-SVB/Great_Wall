8cf66e28f1f79b4db90dda1b2d9b6966f459c08d
AndreFGo <andre7c4@protonmail.com>
2024-01-04 11:11:48 -0300

    Move Shaper to another file and adapt the greatwall

---
+++ src/Shaper.py
@@ -0,0 +1,55 @@
+import random
+from typing import Optional, Union
+from PIL import Image, ImageDraw
+from pathlib import Path
+import math
+
+
+class Shaper:
+    def __init__(self, size: int = 101):
+        self.size = size
+
+        # Create a new image with a black background
+        self.image = Image.new("RGB", (size, size), "black")
+        self.draw = ImageDraw.Draw(self.image)
+
+    @staticmethod
+    def get_first_digit(number: bytes):
+        bytes_1st_digit = int(str(number[0]))
+        integer_1st_digit = str(bytes_1st_digit)[0]
+        return int(integer_1st_digit)
+
+    def draw_regular_shape(self, sides: Union[int, bytes, bytearray] = 3):
+        if isinstance(sides, bytes) or isinstance(sides, bytearray):
+            # If sides is given as bytes it will get the int of the first digit with an offset of 2
+            sides = self.get_first_digit(sides)+2
+        size = self.size
+
+        # Calculate the coordinates for the polygon points
+        center_x, center_y = size // 2, size // 2
+
+        # Calculate the angle step in radians for each vertex
+        angle = 2*math.pi/sides
+
+        # Calculate the vertices
+        vertices = [(int(center_x * math.sin(angle * i)) + center_x,
+                     -int(center_y * math.cos(angle * i)) + center_y)
+                    for i in range(sides)
+                    ]
+
+        # Draw the polygon
+        self.draw.polygon(vertices, outline="white")
+        # Showing for debugging purpose
+        # self.image.show()
+        # self.save_image(sides)
+        # return self.image
+        return self.save_image(sides)
+
+    def save_image(self, name):
+        parent_path = Path(__file__).parent
+        icons_folder_name = f"Icons"
+        filename = f"s{name}-polygon.png"
+        print(filename)
+        file_path = parent_path / Path(icons_folder_name) / Path(filename)
+        self.image.save(file_path, format="png")
+        return file_path
--- src/greatwall.py
+++ src/greatwall.py
@@ -2,40 +2,50 @@ import random
 import argon2
 from typing import Optional, Union
 from src.mnemonic.mnemonic import Mnemonic
+from Shaper import Shaper
 # from user_interface import UserInterface
-from PIL import Image, ImageDraw
-import math
 
 
-class Shaper:
-    omega = 2*math.pi
-
-    def __init__(self, size: int = 101):
-        self.size = size
-
-        # Create a new image with a black background
-        self.image = Image.new("RGB", (size, size), "black")
-        self.draw = ImageDraw.Draw(self.image)
-
-    def draw_regular_shape(self, sides: int = 3):
-        size = self.size
-
-        # Calculate the coordinates for the polygon points
-        center_x, center_y = size // 2, size // 2
-
-        # Calculate the angle step in radians for each vertex
-        angle = self.omega/sides
-
-        # Calculate the vertices
-        vertices = [(int(center_x * math.sin(angle * i)) + center_x,
-                     -int(center_y * math.cos(angle * i)) + center_y)
-                    for i in range(sides)
-                    ]
-
-        # Draw the polygon
-        self.draw.polygon(vertices, outline="white")
-        self.image.show()
-        return self.image
+# class Shaper:
+#     def __init__(self, size: int = 101):
+#         self.size = size
+#
+#         # Create a new image with a black background
+#         self.image = Image.new("RGB", (size, size), "black")
+#         self.draw = ImageDraw.Draw(self.image)
+#
+#     @staticmethod
+#     def get_first_digit(number: bytes):
+#         bytes_1st_digit = int(str(number[0]))
+#         integer_1st_digit = str(bytes_1st_digit)[0]
+#         return int(integer_1st_digit)
+#
+#     def draw_regular_shape(self, sides: Union[int, bytes, bytearray] = 3):
+#         if isinstance(sides, bytes) or isinstance(sides, bytearray):
+#             # If sides is given as bytes it will get the int of the first digit with an offset of 2
+#             sides = self.get_first_digit(sides)+2
+#         size = self.size
+#
+#         # Calculate the coordinates for the polygon points
+#         center_x, center_y = size // 2, size // 2
+#
+#         # Calculate the angle step in radians for each vertex
+#         angle = 2*math.pi/sides
+#
+#         # Calculate the vertices
+#         vertices = [(int(center_x * math.sin(angle * i)) + center_x,
+#                      -int(center_y * math.cos(angle * i)) + center_y)
+#                     for i in range(sides)
+#                     ]
+#
+#         # Draw the polygon
+#         self.draw.polygon(vertices, outline="white")
+#         # Showing for debugging purpose
+#         self.image.show()
+#         filename = f"s{sides}-polygon"
+#         print(filename)
+#         self.image.save(filename+".png")
+#         return self.image
 
 
 class GreatWall:
@@ -197,7 +207,7 @@ class GreatWall:
 
     def get_shape_query(self) -> list:
         self.shuffle_bytes()
-        shuffled_shapes = [Shaper().draw_regular_shape(int(str(bytes_sentence)[0]))
+        shuffled_shapes = [Shaper().draw_regular_shape(bytes_sentence)
                            for bytes_sentence in self.shuffled_bytes]
         listr = "Choose 1, ..., %d for level %d".format(self.tree_arity, self.current_level)
         listr += "%s\n".format("" if not self.current_level else ", choose 0 to go back")
