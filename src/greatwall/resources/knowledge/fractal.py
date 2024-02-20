00e4875b9bd0c607ec41dd8936198b73cd500bd0
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-02-20 23:34:11 +0200

    Revert unrelated commits.

--- src/greatwall/resources/knowledge/fractal.py
+++ src/greatwall/resources/knowledge/fractal.py
@@ -33,10 +33,11 @@ class Fractal:
         self._image_pixels: Optional[np.array] = None
 
     def get_valid_parameter_from_value(self, value: Union[bytes, bytearray]):
-        real_part = "2." + str(int.from_bytes(bytes(list(value)[0:2]), "big"))[::-1]
-        imaginary_part = "0." + str(int.from_bytes(bytes(list(value)[2:4]), "big"))[::-1]
+        parameter = "2."
+        for byte_value in list(value)[:4]:
+            parameter += str(byte_value)
 
-        return complex(float(real_part), float(imaginary_part))
+        return float(parameter)
 
     @property
     def image_pixels(self):
