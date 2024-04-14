a425103df5349f13854e04ebe3c83a68bb54d95f
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-04-14 17:30:13 +0200

    Desgin and initial implement the TKBA param class.

--- src/greatwall/resources/greatwall.py
+++ src/greatwall/resources/greatwall.py
@@ -34,6 +34,84 @@ class DerivationPath(list):
             return ""
 
 
+class TacitKnowledgeParam:
+    """A representation of the tacit knowledge params."""
+
+    ARGON2_SALT: bytes = bytes("00000000000000000000000000000000", "utf-8")
+    NUM_BYTES_FORM: int = 4
+
+    def __init__(self, state: bytes, **kwargs) -> None:
+        self.state: bytes = state
+        self.adjustment_params: dict[str, bytes] = kwargs
+
+        self._value: Optional[bytes] = None
+
+    def get_value(self):
+        """Get the value of the param."""
+        self._value = self._compute_value()
+
+        return self._value
+
+    def _compute_value(self):
+        """Get a valid tacit knowledge value from provided adjustment params."""
+
+        # jth candidate L_(i+1), the state resulting from appending bytes of j
+        # (here, branch_idx_bytes to current state L_i and hashing it)
+        next_state_candidate = self.state
+        for param in self.adjustment_params:
+            tacit_knowledge_param_bytes = self.adjustment_params[param]
+
+            next_state_candidate = low_level.hash_secret_raw(
+                secret=next_state_candidate + tacit_knowledge_param_bytes,
+                salt=self.ARGON2_SALT,
+                time_cost=32,
+                memory_cost=1024,
+                parallelism=1,
+                hash_len=128,
+                type=low_level.Type.I,
+            )
+
+        return next_state_candidate[0 : self.NUM_BYTES_FORM]
+
+
+class FractalTacitKnowledgeParam(TacitKnowledgeParam):
+    """A representation of the fractal tacit knowledge param."""
+
+    def _compute_real_p_value(self, value: bytes):
+        # NOTE: We inverting the order of digits by operation [::-1] on string,
+        # to minimize Benford's law bias.
+        real_p = "2." + str(int.from_bytes(value, "big"))[::-1]
+        return float(real_p)
+
+    def _compute_imag_p_value(self, value: bytes, bytearray]):
+        # NOTE: We inverting the order of digits by operation [::-1] on string,
+        # to minimize Benford's law bias.
+        imag_p = "0." + str(int.from_bytes(value, "big"))[::-1]
+        return float(imag_p)
+
+    def _compute_value(self):
+        if "real_p" in self.adjustment_params:
+            return self._compute_real_p_value(super()._compute_value())
+        elif "imag_p" in self.adjustment_params:
+            return self._compute_real_p_value(super()._compute_value())
+        else:
+            return super()._compute_value()
+
+
+class FormosaTacitKnowledgeParam(TacitKnowledgeParam):
+    """A representation of the formosa tacit knowledge param."""
+
+    def _compute_value(self) -> bytes:
+        return super()._compute_value()
+
+
+class ShapeTacitKnowledgeParam(TacitKnowledgeParam):
+    """A representation of the shape tacit knowledge param."""
+
+    def _compute_value(self) -> bytes:
+        return super()._compute_value()
+
+
 class GreatWall:
     ARGON2_SALT: bytes = bytes("00000000000000000000000000000000", "utf-8")
     NUM_BYTES_FORM: int = 4
@@ -168,7 +246,7 @@ class GreatWall:
         self._saved_states[self._derivation_path.copy()] = self.state
 
     def update_with_long_hash(self):
-        """ Update the state with the its hash taking presumably a long time."""
+        """Update the state with the its hash taking presumably a long time."""
         for i in range(self.tlp_param):
             print("iteration #", i + 1, " of TLP:")
             self.state = low_level.hash_secret_raw(
@@ -182,7 +260,7 @@ class GreatWall:
             )
 
     def update_with_quick_hash(self):
-        """ Update the state with the its hash taking presumably a quick time."""
+        """Update the state with the its hash taking presumably a quick time."""
         self.state = low_level.hash_secret_raw(
             secret=self.state,
             salt=self.ARGON2_SALT,
@@ -198,38 +276,6 @@ class GreatWall:
         self.shuffled_arity_indxes = [arity_idx for arity_idx in range(self.tree_arity)]
         random.shuffle(self.shuffled_arity_indxes)
 
-    def _get_tacit_knowledge_param_from(
-        self, branch_idx: int, tacit_knowledge_param: Optional[str] = None
-    ):
-        """Get a valid tacit knowledge param from provided arguments."""
-        branch_idx_bytes = branch_idx.to_bytes(length=4, byteorder="big")
-
-        # jth candidate L_(i+1), the state resulting from appending bytes of j
-        # (here, branch_idx_bytes to current state L_i and hashing it)
-        next_state_candidate = low_level.hash_secret_raw(
-            secret=self.state + branch_idx_bytes,
-            salt=self.ARGON2_SALT,
-            time_cost=32,
-            memory_cost=1024,
-            parallelism=1,
-            hash_len=128,
-            type=low_level.Type.I,
-        )
-
-        if tacit_knowledge_param is not None:
-            tacit_knowledge_param_bytes = tacit_knowledge_param.encode(encoding="utf-8")
-            next_state_candidate = low_level.hash_secret_raw(
-                secret=next_state_candidate + tacit_knowledge_param_bytes,
-                salt=self.ARGON2_SALT,
-                time_cost=32,
-                memory_cost=1024,
-                parallelism=1,
-                hash_len=128,
-                type=low_level.Type.I,
-            )
-
-        return next_state_candidate[0 : self.NUM_BYTES_FORM]
-
     def get_fractal_query(self) -> list:
         if self._derivation_path in self._saved_fractals:
             return self._saved_fractals[self._derivation_path]
@@ -238,11 +284,15 @@ class GreatWall:
             shuffled_fractals = [
                 self.fractal.update(
                     func_type=self.fractal.func_type,
-                    real_p=self.fractal.get_valid_real_p_from(
-                        self._get_tacit_knowledge_param_from(arity_idx, "real_p")
+                    real_p=FractalTacitKnowledgeParam(
+                        self.state,
+                        branch_idx=arity_idx.to_bytes(length=4, byteorder="big"),
+                        param1="real_p".encode(encoding="utf-8"),
                     ),
-                    imag_p=self.fractal.get_valid_imag_p_from(
-                        self._get_tacit_knowledge_param_from(arity_idx, "imag_p")
+                    imag_p=FractalTacitKnowledgeParam(
+                        self.state,
+                        branch_idx=arity_idx.to_bytes(length=4, byteorder="big"),
+                        param2="imag_p".encode(encoding="utf-8"),
                     ),
                 )
                 for arity_idx in self.shuffled_arity_indxes
