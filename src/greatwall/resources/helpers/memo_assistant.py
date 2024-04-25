7b9a27db6d975948a7b26b495382d00db031d939
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-04-25 13:24:14 +0200

    Initial implementation of memorization assistant.

---
+++ src/greatwall/resources/helpers/memo_assistant.py
@@ -0,0 +1,17 @@
+from fsrs import Card, FSRS, Rating, State
+from datetime import datetime
+
+
+memo_algorithm = FSRS()
+card = Card()
+
+now = datetime.utcnow()
+scheduling_cards = memo_algorithm.repeat(card, now)
+
+print(card)
+print(scheduling_cards)
+
+card = scheduling_cards[Rating.Again].card
+review_log = scheduling_cards[Rating.Good].review_log
+print(review_log.state)
+print(card.due)
