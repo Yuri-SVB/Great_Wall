85d14252383fbeb4e1b2a069d93664b09036284f
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-05-02 22:51:40 +0200

    Implement backend of the memorization.

--- src/greatwall_desktop/memo_assistant.py
+++ src/greatwall_desktop/memo_assistant.py
@@ -1,26 +1,59 @@
-from fsrs import Card, FSRS, Rating, State
-from datetime import datetime
+from datetime import datetime, timezone
+from typing import Any, Optional
+
+from fsrs import FSRS, Card, Rating, State
 
 
-memo_algorithm = FSRS()
-card = Card()
+class MemoCard:
+    """A card that contains knowledge and implements spaced repetition algorithm."""
 
-now = datetime.utcnow()
-scheduling_cards = memo_algorithm.repeat(card, now)
+    def __init__(self, knowledge) -> None:
+        self._knowledge = knowledge
+        self._algorithm = FSRS()
+        self._card = Card()
+        self._log = None
 
-print(card)
-print(scheduling_cards)
+    def rate_card(self, rating: str):
+        """Review the card knowledge now and rate it.
 
-card = scheduling_cards[Rating.Again].card
-review_log = scheduling_cards[Rating.Good].review_log
-print(review_log.state)
-print(card.due)
+        Args:
+            rating (str): The rate of the card knowledge. It can take rate values
+                'again', 'hard', 'good' and 'easy'.
+        """
+        now = datetime.now(timezone.utc)
+        rates = {
+            "again": Rating.Again,
+            "hard": Rating.Hard,
+            "good": Rating.Good,
+            "easy": Rating.Easy,
+        }
 
+        scheduling_cards = self._algorithm.repeat(self._card, now)
+        self._card = scheduling_cards[rates[rating]].card
+        self._log = scheduling_cards[rates[rating]].review_log
 
-class MeomCard():
+    @property
+    def knowledge(self) -> Any:
+        return self._knowledge
 
-    def __init__(self) -> None:
-        memo_algorithm = FSRS()
-        card = Card()
+    @property
+    def due(self) -> Optional[datetime]:
+        return self._card.due
 
+    @property
+    def state(self) -> Optional[int]:
+        if self._log:
+            return self._log.state
+        else:
+            return State.New
 
+    def __str__(self) -> str:
+        return (
+            "Memorization Card for Knowledge: {}; with state: {} and due at: {}.".format(
+                self.knowledge, self.state, self.due
+            )
+        )
+
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        return f"{class_name}(knowledge={self.knowledge})"
